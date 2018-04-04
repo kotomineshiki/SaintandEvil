937594d5d6186535ab826da9aff7b6cedbdb0a6c
yiqu zhu <kotomineshiki@outlook.com>
2018-04-04 18:51:36 +0800

    添加ui版
    引入了新的bug

--- saintandevil/Assets/Scripts/SSDirector.cs
+++ saintandevil/Assets/Scripts/SSDirector.cs
@@ -34,5 +34,6 @@ public class SSDirector : System.Object {
 public interface ISceneController
 {
     void LoadResources();
+    void restart();
 
 }
--- saintandevil/Assets/Scripts/gameController.cs
+++ saintandevil/Assets/Scripts/gameController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class gameController : MonoBehaviour ,ISceneController{
     //public  Camera viewport;
-	// Use this for initialization
+    // Use this for initialization
 
+    
+    public string gameState="";
     void Awake()
     {
         SSDirector director = SSDirector.getInstance();
@@ -16,9 +18,33 @@ public class gameController : MonoBehaviour ,ISceneController{
 
 
     }
+    void OnGUI()
+    {
+        if (gameState == "Win")
+        {
+            GUI.Label(new Rect(Screen.width * 0.5f, Screen.height * 0.5f, 100, 30), "WIN");
+            if(GUI.Button(new Rect(Screen.width * 0.7f, Screen.height * 0.5f, 100, 30),"REstart"))
+            {
+                restart();
+            }
+        }
+        if (gameState == "Lose")
+        {
+            GUI.Label(new Rect(Screen.width * 0.5f, Screen.height * 0.5f, 100, 30), "saint KILLED");
+            if (GUI.Button(new Rect(Screen.width * 0.7f, Screen.height * 0.5f, 100, 30),"REstart"))
+            {
+                restart();
+            }
+        }
+        
+    }
     public void LoadResources()
     {
         this.GetComponent<GenGameObject>().generate();
+    }
+    public void restart()
+    {
+
     }
     // Update is called once per frame
     void Update () {
@@ -51,9 +77,11 @@ public class gameController : MonoBehaviour ,ISceneController{
         {
             case 1:
                 Debug.Log("Wins");
+                gameState = "Win";
                 break;
             case 2:
                 Debug.Log("Saints defeated");
+                gameState = "Lose";
                 break;
 
         }
